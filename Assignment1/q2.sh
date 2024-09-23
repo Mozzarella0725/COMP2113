ae5d00da93de5c17103319ad5d48ed0a0ccc5aba
Zhao Kexin <h3584536@academy11.cs.hku.hk>
2024-09-23 14:08:06 +0800

    q2 is done, but need to test

---
+++ Assignment1/q2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ ! -f "students.txt" ]; then
+	echo "File students.txt not found."
+	exit 0
+fi
+
+while IFS= read -r line; do
+	if [[ $line == "Name"* ]]; then
+		name=${line:6}
+		total=0
+		# echo "Now the student is $name"
+	elif [[ $line == "A"* ]]; then
+		score=${line:4}
+		total=$((total + score))
+		# echo "The grade this time is $score, and in total get $total"
+	elif [[ $line == "---" ]]; then
+		average=$((total / 4))
+		echo "Student name: $name, Average core: $average"
+	fi
+done < students.txt
