d2331654e42127c1a4bba73e7021d3ef877e78d4
Zhao Kexin <142424952+Mozzarella0725@users.noreply.github.com>
2024-09-23 11:13:44 +0800

    Update q1_draft.sh

--- Assignment1/q1_draft.sh
+++ Assignment1/q1_draft.sh
@@ -1,1 +1,13 @@
-
+#!/bin/bash
+echo “Enter the number /for checking: “
+read n
+x=0
+for (( i = 0; i < ${#n}; i++ ))
+do 
+  d=${n:$i:1}
+  if [ "$d%2" == 0 ]
+  then
+      x=$((x + d)
+  else
+      
+      
