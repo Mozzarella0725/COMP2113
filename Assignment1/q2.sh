e4b9859508a9f48742e3172605032fde92d6a383
Zhao Kexin <142424952+Mozzarella0725@users.noreply.github.com>
2024-09-23 14:42:39 +0800

    Update q2.sh_according to the input file format
    the test case is not ended with ---
    thus need to echo the grades of the last students after the while loop

--- Assignment1/q2.sh
+++ Assignment1/q2.sh
@@ -9,14 +9,14 @@ while IFS= read -r line; do
 	if [[ $line == "Name"* ]]; then
 		name=${line:6}
 		total=0
-		echo "Now the student is $name"
+		#echo "Now the student is $name"
 	elif [[ $line == "A"* ]]; then
 		score=${line:4}
 		total=$((total + score))
-		# echo "The grade this time is $score, and in total get $total"
-	elif [[ $line == "---" ]]; then
 		average=$((total / 4))
+		#echo "The grade this time is $score, and in total get $total"
+	elif [[ $line == "---" ]]; then
 		echo "Student name: $name, Average score: $average"
 	fi
-
 done < students.txt
+echo "Student name: $name, Average score: $average"
