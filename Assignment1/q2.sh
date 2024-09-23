6909fd243ef5b162ae19671cc23d755777466425
Zhao Kexin <h3584536@academy11.cs.hku.hk>
2024-09-23 14:19:31 +0800

    revised q2

--- Assignment1/q2.sh
+++ Assignment1/q2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -f "students.txt" ]; then
-	echo "File students.txt not found."
+	echo "File students.txt not found"
 	exit 0
 fi
 
@@ -9,13 +9,14 @@ while IFS= read -r line; do
 	if [[ $line == "Name"* ]]; then
 		name=${line:6}
 		total=0
-		# echo "Now the student is $name"
+		echo "Now the student is $name"
 	elif [[ $line == "A"* ]]; then
 		score=${line:4}
 		total=$((total + score))
 		# echo "The grade this time is $score, and in total get $total"
 	elif [[ $line == "---" ]]; then
 		average=$((total / 4))
-		echo "Student name: $name, Average core: $average"
+		echo "Student name: $name, Average score: $average"
 	fi
+
 done < students.txt
