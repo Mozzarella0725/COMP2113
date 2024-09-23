3bc4e1f0c7c9f952f498bd2a7de958ba92b74aa5
Zhao Kexin <142424952+Mozzarella0725@users.noreply.github.com>
2024-09-23 13:47:07 +0800

    Update q1.sh_annotations about the process

--- Assignment1/q1.sh
+++ Assignment1/q1.sh
@@ -9,13 +9,13 @@ do
   if (( $i % 2 == 1 ))
   then
       x=$((x + d))
-      echo "d=$d x=$x"
+      # echo "d=$d x=$x"
   else
       d2=$((d * 2))
       a=$((d2 % 10))
       b=$((d2 / 10))
       x=$((x + a + b))
-      echo "d=$d d2=$d2 a=$a b=$b x=$x"   
+      # echo "d=$d d2=$d2 a=$a b=$b x=$x"   
   fi
 done
 if (( $x % 10 == 0 ))
